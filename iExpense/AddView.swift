74eda492eeda1c650fc36251c609af4c1fdce903
JonasMahlburg <darrylmanga@yahoo.com>
2025-10-29 12:51:35 +0100

    feat: added Expenses will be now saved

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -31,6 +31,12 @@ struct AddView: View {
                     .keyboardType(.decimalPad)
             }
             .navigationTitle("Add new expense")
+            .toolbar{
+                Button("Safe"){
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                }
+            }
         }
     }
 }
