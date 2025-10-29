15ad08ca07e3d225f6aa0f27680391edbc1cd171
JonasMahlburg <darrylmanga@yahoo.com>
2025-10-29 14:10:47 +0100

    feat: Expenses List now shows: name, type and amount

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct AddView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
     @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = 0.0
@@ -35,6 +38,7 @@ struct AddView: View {
                 Button("Safe"){
                     let item = ExpenseItem(name: name, type: type, amount: amount)
                     expenses.items.append(item)
+                    dismiss()
                 }
             }
         }
